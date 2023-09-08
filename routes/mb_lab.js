e786233e3ea9cbe6e1dea7f4018b01be9971e675
DevGame <t.seagames@hotmail.com>
2023-09-08 16:29:02 +0700

    create mb_lab_room

--- routes/mb_lab.js
+++ routes/mb_lab.js
@@ -1,15 +1,13 @@
 const express = require("express");
 var router = express.Router();
-const mysql = require("mysql2");
 const db = require('./database')
-const app = express();
 
 
 router.get("/mb_lab_room", function (req, res, next) {
   // res.render('index', { title: 'Express' });
   db.execute(`SELECT * FROM mb_room`)
   .then(([data, fields]) => {
-    res.json({ data: data })
+    res.json({ data })
   })
   .catch((error) => {
     console.log(error);
