a1cbfd9216bd55e98d5e16cc3d7e2b91e1b44ba5
DevGame <t.seagames@hotmail.com>
2023-09-12 10:52:26 +0700

    C:/Program Files/Git/bookLabRoom can insert from backend

--- routes/mb_lab.js
+++ routes/mb_lab.js
@@ -14,64 +14,33 @@ router.get("/mb_lab_room", function (req, res, next) {
 });
 
 router.post("/bookLabRoom", (req, res) => {
-  return new Promise((resolve) => {
-    const {
-      ac_name,
-      name,
-      phone,
-      zone,
-      floor,
-      where_lab,
-      start_date,
-      endtime,
-      timebook,
-      appove_status,
-      appove_ac_name,
-    } = req.body;
-    console.log(
-      ac_name,
-      name,
-      phone,
-      zone,
-      floor,
-      where_lab,
-      start_date,
-      endtime,
-      timebook,
-      appove_status,
-      appove_ac_name
-    );
-    let sql =
-      "INSERT INTO from SET id=?, ac-name=?, name=?, num_in_team=?, phone=?, where_lab=?, start_date=?, endtime=?, timebook=?";
-    db.execute(sql, [
-      ac_name,
-      name,
-      phone,
-      where_lab,
-      start_date,
-      endtime,
-      timebook,
-    ]),
-      (err, result) => {
-        if (err) {
-          console.log(err + " at bookLabRoom");
+  const {
+    ac_name,
+    name,
+    num_in_team,
+    phone,
+    zone,
+    floor,
+    where_lab,
+    start_date,
+    endtime,
+    timebook,
+    appove_status,
+    appove_ac_name,
+  } = req.body;
 
-          resolve(err + " at bookLabRoom");
-        } else {
-          resolve("ok");
-        }
-      };
-  });
-});
+  let sql = 'INSERT INTO book_lab set  ac_name=?, name=?, num_in_team=?, phone=?, where_lab=?, start_date=?, endtime=?, timebook=?'
+  db.execute(sql, [ac_name, name, num_in_team, phone, where_lab, start_date, endtime, timebook,], (err, result) => {
+    if (err) {
+      console.log(err + "bookLabRoom")
+      req.msg = 'err'
+    }
+    else {
+      req.msg = 'ok'
+    }
+  })
 
-// function bookLabRoom(data) {
-//   return new Promise((resolve) => {
-//     let sql = 'INSERT INTO `book_lab`(`id`, `ac-name`, `name`, `num_in_team`, `phone`, `where_lab`, `start_date`, `endtime`, `timebook`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]','[value-9]')'
-//   })
-// }
+})
 
 module.exports = router;
 
-// app.post('/mbroom_send_form', mbroom_send_form, (req, res) => {
-//     res.json({ msg: req.msg, payload: req.data })
-// })
