642311f3c8489d5ec500401dac01fca3c7aec0ea
DevGame <t.seagames@hotmail.com>
2023-09-19 08:59:57 +0700

    fix insert value to db

--- routes/mb_lab.js
+++ routes/mb_lab.js
@@ -24,13 +24,24 @@ router.post("/bookLabRoom", (req, res) => {
     where_lab,
     start_date,
     endtime,
-    timebook,
     appove_status,
     appove_ac_name,
   } = req.body;
 
-  let sql = 'INSERT INTO book_lab set  ac_name=?, name=?, num_in_team=?, phone=?, where_lab=?, start_date=?, endtime=?, timebook=?'
-  db.execute(sql, [ac_name, name, num_in_team, phone, where_lab, start_date, endtime, timebook,], (err, result) => {
+  console.log(ac_name,
+    name,
+    num_in_team,
+    phone,
+    zone,
+    floor,
+    where_lab,
+    start_date,
+    endtime,
+    appove_status,
+    appove_ac_name,)
+
+  let sql = 'INSERT INTO book_lab set  ac_name=?, name=?, num_in_team=?, phone=?, where_lab=?, start_date=?, endtime=?'
+  db.execute(sql, [ac_name, name, num_in_team, phone, where_lab, start_date, endtime], (err, result) => {
     if (err) {
       console.log(err + "bookLabRoom")
       req.msg = 'err'
