2dcce125c77b05bd36181365247805908837ca12
DevGame <t.seagames@hotmail.com>
2023-09-25 15:57:29 +0700

    format datetime to date and time

--- routes/mb_lab.js
+++ routes/mb_lab.js
@@ -48,8 +48,8 @@ router.post("/bookLabRoom", (req, res) => {
 const fs = require('fs').promises;
 const path = require('path');
 const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
-const {google} = require('googleapis');
+const { authenticate } = require('@google-cloud/local-auth');
+const { google } = require('googleapis');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
@@ -116,30 +116,46 @@ async function authorize() {
  * Lists the next 10 events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-async function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  const res = await calendar.events.list({
-    calendarId: 'primary',
-    timeMin: new Date().toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  });
-  const events = res.data.items;
-  if (!events || events.length === 0) {
-    console.log('No upcoming events found.');
-    return;
-  }
-  //console.log('Upcoming 10 events:');
-  events.map((event, i) => {
-    const start = event.start.dateTime || event.start.date;
-    console.log(`${start} - ${event.summary}`);
-  });
-}
+// async function listEvents(auth) {
+//   const calendar = google.calendar({ version: 'v3', auth });
+//   const res = await calendar.events.list({
+//     calendarId: 'primary',
+//     timeMin: new Date().toISOString(),
+//     maxResults: 10,
+//     singleEvents: true,
+//     orderBy: 'startTime',
+//   });
+//   const events = res.data.items;
+//   if (!events || events.length === 0) {
+//     console.log('No upcoming events found.');
+//     return;
+//   }
+//   //console.log('Upcoming 10 events:');
+//   events.map((event, i) => {
+//     const start = event.start.dateTime || event.start.date;
+//     console.log(`${start} - ${event.summary}`);
+//   });
+// }
 
 //authorize().then(listEvents).catch(console.error);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function dateFormat(date) {
+  const dateTimeString = date; // วันที่และเวลาในรูปแบบ ISO 8601
+  const dateTime = new Date(dateTimeString); // แปลงเป็นวัตถุ Date
+  const day = dateTime.getDate().toString().padStart(2, '0'); // วัน
+  const month = (dateTime.getMonth() + 1).toString().padStart(2, '0'); // เดือน (เพิ่ม 1 เนื่องจากมกราคมเริ่มที่ 0)
+  const year = dateTime.getFullYear(); // ปี
+  const formattedDate = `${day}-${month}-${year + 543}`;
+  return formattedDate
+}
+
+function formatTime(dateTimeString) {
+  const dateTime = new Date(dateTimeString);
+  const hours = dateTime.getHours().toString().padStart(2, '0');
+  const minutes = dateTime.getMinutes().toString().padStart(2, '0');
+  return `${hours}:${minutes}`;
+}
 
 async function addEventToCalendar(name, startdate, enddate) {
   const auth = await authenticate({
@@ -148,19 +164,21 @@ async function addEventToCalendar(name, startdate, enddate) {
   });
 
   const calendar = google.calendar({ version: 'v3', auth });
+  const end = new Date(enddate);
+  const start = startdate.split("T")[0]; // ดึงเอาเวลาออก 
+  end.setDate(end.getDate() + 1); // เพิ่ม 1 วันใน end
 
-  // Define the event details
+
   const event = {
-    summary: `${name} ${startdate} - ${enddate}`, // Event summary (title)
-    description: 'This is a sample event description.', // Event description
+    summary: `${name}`,
+    description: `${formatTime(startdate)} - ${formatTime(enddate)}`,
     start: {
-      dateTime: startdate, // Start date and time in ISO 8601 format
-      timeZone: 'UTC', // Time zone for the start time
+      date: start
     },
     end: {
-      dateTime: enddate, // End date and time in ISO 8601 format
-      timeZone: 'UTC', // Time zone for the end time
+      date: end.toISOString().split('T')[0], // ใช้ endDate ที่แก้ไขแล้ว
     },
+    colorId: '3', // ตั้งค่ารหัสสีให้กับกิจกรรม
   };
 
   // Insert the event into the primary calendar
@@ -172,7 +190,7 @@ async function addEventToCalendar(name, startdate, enddate) {
   console.log('Event created:', response.data.htmlLink);
 }
 
-addEventToCalendar('ODIN SON Inwza', '2023-09-10T23:00:00', '2023-09-11T08:00:00').catch(console.error);
+addEventToCalendar('ODIN SON Inwza', '2023-09-28T23:00:00', '2023-09-29T08:00:00').catch(console.error);
 //addEventToCalendar('ODIN SON', '2023-10-25T10:00:00', '2023-10-27T16:00:00').catch(console.error);
 //2023-09-20T17:07:00.038Z
 
